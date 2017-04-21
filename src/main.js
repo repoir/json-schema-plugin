6abd8bbae66362bd8a9d75c87e6998e204cc8fb9
Cory Roloff <cory@brothersroloff.com>
2017-04-21 14:10:22 -0500

    Initial commit of the plugin source

---
+++ src/main.js
@@ -0,0 +1,81 @@
+import Ajv from 'ajv';
+import formatAjvError from './format-ajv-error';
+import fs from 'fs';
+import path from 'path';
+
+export const schema = {
+	type: 'array',
+	items: {
+		$ref: '#/definitions/item'
+	},
+	definitions: {
+		item: {
+			properties: {
+				file: {
+					type: 'string'
+				},
+				schema: {
+					type: 'string'
+				}
+			},
+			required: ['file', 'schema']
+		}
+	}
+};
+
+export function test (ruleConfig, program) {
+	return Promise.all(ruleConfig.map(config => {
+		return new Promise(validate.bind(null, config, program));
+	}))
+	.then(results => results.reduce((accumulator, value) => {
+		return accumulator.concat(value);
+	}, []));
+}
+
+function validate (config, program, resolve, reject) {
+	const ajv = new Ajv({ allErrors: true });
+	const { file, schema } = config;
+	const loadedFile = load(file);
+	const loadedSchema = load(schema, path.dirname(program.config));
+
+	if (loadedFile.error) return resolve([loadedFile.error]);
+	if (loadedSchema.error) return resolve([loadedSchema.error]);
+
+	if (!ajv.validate(loadedSchema.data, loadedFile.data)) {
+		resolve(ajv.errors.map(error => {
+			return Object.assign({}, error.params, {
+				message: formatAjvError(error, file),
+				file
+			});
+		}));
+	}
+
+	resolve([]);
+}
+
+function load (file, relativePath) {
+	const resolvedFile = !relativePath ?
+		path.resolve(file) :
+		path.resolve(path.relative(process.cwd(), path.join(relativePath, file)));
+
+	if (!fs.existsSync(resolvedFile)) {
+		return {
+			error: { message: `File does not exist: ${file}` },
+			data: null
+		};
+	}
+
+	try {
+		return {
+			error: null,
+			data: JSON.parse(fs.readFileSync(resolvedFile, 'utf8'))
+		};
+	} catch (error) {
+		return {
+			error: {
+				message: `Invalid JSON: ${file}`
+			},
+			data: null
+		};
+	}
+}
---
+++ src/main.spec.js
@@ -0,0 +1,141 @@
+import fs from 'fs';
+import path from 'path';
+import { test } from './main';
+
+jest.mock('fs');
+
+describe('json-schema', () => {
+	afterEach(() => {
+		jest.resetAllMocks();
+	});
+
+	it('should resolve with an empty array when given a file that matches the schema', async () => {
+		fs.existsSync.mockReturnValue(true);
+
+		fs.readFileSync.mockReturnValueOnce(JSON.stringify({
+			foo: 'bar'
+		}));
+
+		fs.readFileSync.mockReturnValueOnce(JSON.stringify({
+			properties: {
+				foo: { type: 'string' }
+			},
+			required: ['foo']
+		}));
+
+		const result = await test([ {
+			file: 'foobar.json',
+			schema: 'foobar.schema.json'
+		} ], { config: '/the/path' });
+
+		expect(result).toEqual([]);
+	});
+
+	it('should resolve with the list of problems when given a file not matching the schema', async () => {
+		fs.existsSync.mockReturnValue(true);
+
+		fs.readFileSync.mockReturnValueOnce(JSON.stringify({}));
+
+		fs.readFileSync.mockReturnValueOnce(JSON.stringify({
+			type: 'object',
+			properties: {
+				foo: { type: 'string' },
+				bar: { type: 'string' }
+			},
+			required: ['foo', 'bar']
+		}));
+
+		const result = await test([ {
+			file: 'foobar.json',
+			schema: 'foobar.schema.json'
+		} ], { config: '/the/path' });
+
+		expect(result).toEqual([
+			{
+				file: 'foobar.json',
+				message: 'foobar.json should have required property \'foo\'',
+				missingProperty: 'foo'
+			},
+			{
+				file: 'foobar.json',
+				message: 'foobar.json should have required property \'bar\'',
+				missingProperty: 'bar'
+			}
+		]);
+	});
+
+	it('should resolve with the list of problems when given a file that does not exist', async () => {
+		fs.existsSync.mockReturnValue(false);
+
+		const result = await test([ {
+			file: 'foobar.json',
+			schema: 'foobar.schema.json'
+		} ], { config: '/the/path' });
+
+		expect(result).toEqual([ {
+			message: 'File does not exist: foobar.json'
+		} ]);
+	});
+
+	it('should resolve with the list of problems when given a file with invalid JSON', async () => {
+		fs.existsSync.mockReturnValue(true);
+
+		fs.readFileSync.mockReturnValue('{');
+
+		const result = await test([ {
+			file: 'foobar.json',
+			schema: 'foobar.json'
+		} ], { config: '/the/path' });
+
+		expect(result).toEqual([ {
+			message: 'Invalid JSON: foobar.json'
+		} ]);
+	});
+
+	it('should resolve with the list of problems when given a schema that does not exist', async () => {
+		fs.existsSync.mockReturnValueOnce(true);
+
+		fs.readFileSync.mockReturnValueOnce(JSON.stringify({}));
+
+		fs.existsSync.mockReturnValueOnce(false);
+
+		const result = await test([ {
+			file: 'foobar.json',
+			schema: 'foobar.schema.json'
+		} ], { config: '/the/path' });
+
+		expect(result).toEqual([ {
+			message: 'File does not exist: foobar.schema.json'
+		} ]);
+	});
+
+	it('should resolve with the list of problems when given a schema with invalid JSON', async () => {
+		fs.existsSync.mockReturnValue(true);
+
+		fs.readFileSync.mockReturnValueOnce(JSON.stringify({}));
+
+		fs.readFileSync.mockReturnValueOnce('{');
+
+		const result = await test([ {
+			file: 'foobar.json',
+			schema: 'foobar.schema.json'
+		} ], { config: '/the/path' });
+
+		expect(result).toEqual([ {
+			message: 'Invalid JSON: foobar.schema.json'
+		} ]);
+	});
+
+	it('should resolve schemas relative to the repoir file', async () => {
+		fs.existsSync.mockReturnValue(true);
+
+		await test([ {
+			file: 'foobar.json',
+			schema: 'foobar.schema.json'
+		} ], { config: '../fake-config-dir/foobar/.repoir.json' });
+
+		const result = path.relative(process.cwd(), fs.readFileSync.mock.calls[1][0]);
+
+		expect(result).toEqual('../fake-config-dir/foobar/foobar.schema.json');
+	});
+});
